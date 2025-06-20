ee18f08f0035657e471f41c9f3ce82f22b707531
Jorge-Fernandez-Varela <jorgezvg@protonmail.com>
2025-06-20 13:48:17 -0600

    Añadida etiqueta script en HTML y corregidos errores del JS

---
+++ Dynamics/enmaquetado.js
@@ -0,0 +1,52 @@
+let contenedorInicio = document.getElementById("contenedorInicio");
+let contenedorPlaylist = document.getElementById("contenedorPlaylist");
+
+let botonInicio = document.getElementById("botonInicio")
+botonInicio.addEventListener("click", ()=>{
+    contenedorInicio.style.display = "block";
+    contenedorPlaylist.style.display = "none";
+    contenedorMix.style.display = "none";
+    contenedorAjustes.style.display = "none";
+    contenedorCreditos.style.display = "none";
+});
+
+
+let botonPlaylist = document.getElementById("botonPlaylist").addEventListener("click", ()=>{
+    contenedorInicio.style.display = "none";
+    contenedorPlaylist.style.display = "block";
+    contenedorMix.style.display = "none";
+    contenedorAjustes.style.display = "none";
+    contenedorCreditos.style.display = "none";
+    
+});
+
+/*
+let contenedorMix = document.getElementById("contenedorMiMix")
+document.getElementById("botonMiMix").addEventListener("click", ()=>{
+    contenedorInicio.style.display = "none";
+    contenedorPlaylist.style.display = "none";
+    contenedorMix.style.display = "block";
+    contenedorAjustes.style.display = "none";
+    contenedorCreditos.style.display = "none";
+    
+});
+
+let contenedorAjustes = document.getElementById("contenedorAjustes")
+document.getElementById("ajustes").addEventListener("click", ()=>{
+    contenedorInicio.style.display = "none";
+    contenedorPlaylist.style.display = "none";
+    contenedorMix.style.display = "none";
+    contenedorAjustes.style.display = "block";
+    contenedorCreditos.style.display = "none";
+    
+});
+
+let contenedorCreditos = document.getElementById("contenedorCreditos")
+document.getElementById("creditos").addEventListener("click", ()=>{
+    contenedorInicio.style.display = "none";
+    contenedorPlaylist.style.display = "none";
+    contenedorMix.style.display = "none";
+    contenedorAjustes.style.display = "none";
+    contenedorCreditos.style.display = "block";
+    
+});*/
