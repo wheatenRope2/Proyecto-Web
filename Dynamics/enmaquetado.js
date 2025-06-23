d92adbd124344d24cb6415af182c746f8c43e975
DiegoLopezMayen <324250334@alumno.enp.unam.mx>
2025-06-23 10:30:24 -0600

    Se agregó el css de la intefaz

--- Dynamics/enmaquetado.js
+++ Dynamics/enmaquetado.js
@@ -5,37 +5,24 @@ let botonInicio = document.getElementById("botonInicio")
 botonInicio.addEventListener("click", ()=>{
     contenedorInicio.style.display = "block";
     contenedorPlaylist.style.display = "none";
-    contenedorMix.style.display = "none";
     contenedorAjustes.style.display = "none";
     contenedorCreditos.style.display = "none";
+    document.getElementsByClassName("fondo").style.backgroundImage = url('FondoInicio.jpg');
 });
 
 
 let botonPlaylist = document.getElementById("botonPlaylist").addEventListener("click", ()=>{
     contenedorInicio.style.display = "none";
     contenedorPlaylist.style.display = "block";
-    contenedorMix.style.display = "none";
     contenedorAjustes.style.display = "none";
     contenedorCreditos.style.display = "none";
-    
-});
-
-/*
-let contenedorMix = document.getElementById("contenedorMiMix")
-document.getElementById("botonMiMix").addEventListener("click", ()=>{
-    contenedorInicio.style.display = "none";
-    contenedorPlaylist.style.display = "none";
-    contenedorMix.style.display = "block";
-    contenedorAjustes.style.display = "none";
-    contenedorCreditos.style.display = "none";
-    
+    document.getElementsByClassName("fondo").style.backgroundImage = url('FondoPlaylist.jpg');
 });
 
 let contenedorAjustes = document.getElementById("contenedorAjustes")
 document.getElementById("ajustes").addEventListener("click", ()=>{
     contenedorInicio.style.display = "none";
     contenedorPlaylist.style.display = "none";
-    contenedorMix.style.display = "none";
     contenedorAjustes.style.display = "block";
     contenedorCreditos.style.display = "none";
     
@@ -45,8 +32,7 @@ let contenedorCreditos = document.getElementById("contenedorCreditos")
 document.getElementById("creditos").addEventListener("click", ()=>{
     contenedorInicio.style.display = "none";
     contenedorPlaylist.style.display = "none";
-    contenedorMix.style.display = "none";
     contenedorAjustes.style.display = "none";
     contenedorCreditos.style.display = "block";
     
-});*/
+});
